05ff1d569591769c0512402592f81acd4c03bc63
ppiotrlach <lachpio999@gmail.com>
2022-03-30 13:18:05 +0200

    dynamic threads creation

--- etap1/thread.cpp
+++ etap1/thread.cpp
@@ -13,17 +13,19 @@ using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
 using std::chrono::system_clock;
 //-----------------
 
-const int BOARD_WIDTH = 0.5 * 21;
+const int FPS = 30;
+const int BOARD_WIDTH = 21;
 const char EMPTY_PLACE = '.';
 const char BG_CHAR = ' ';
 const int VELOCITY_FACTOR = 1;
 const float VELOCITY_MULTIPLIER = 0.05 * VELOCITY_FACTOR;
 
-const int CARS_NR2 = 4;
-const int CONST_VELOCITY = 500;
+const int CARS_NR1 = 10;
+const int CARS_NR2 = 10;
 
-const int LAPS_NUMBER = 3;
+const int LAPS_NUMBER = 1;
 
+bool car_finished_road = false;
 bool do_work = true;
 
 // const int RIGHT_CORNER = BOARD_WIDTH * 2 / 3 + 1;
@@ -58,7 +60,7 @@ public:
         bool is_after_first_turn = false;
         int random_road = rand() % 4 + 1;
 
-        int rand_time =  rand() % 10000 + 1;
+        int rand_time = rand() % 10000 + 1;
 
         for (int t = 0; t < rand_time; t++)
         {
@@ -76,7 +78,7 @@ public:
             if (is_after_first_turn || random_road == 1)
             {
                 is_after_first_turn = true;
-                for (int i = LEFT_CORNER + 1; i <= RIGHT_CORNER; i++) //go right
+                for (int i = LEFT_CORNER + 1; i <= RIGHT_CORNER; i++) // go right
                 {
                     if (!do_work)
                         break;
@@ -169,7 +171,7 @@ public:
                 if (!do_work)
                     break;
 
-                for (int i = LEFT_CORNER + 1; i <= RIGHT_CORNER; i++) //go right
+                for (int i = LEFT_CORNER + 1; i <= RIGHT_CORNER; i++) // go right
                 {
                     if (!do_work)
                         break;
@@ -240,14 +242,16 @@ public:
                 }
             }
             board[LEFT_CORNER][0] = EMPTY_PLACE;
-            sleep(rand() % 5 + 1);
-            int c = rand() % 25 + 65;        //65-90
-            int random_v = rand() % 75 + 75; //75-150
+            // sleep(rand() % 5 + 1);
+            // int c = rand() % 25 + 65;        // 65-90
+            // int random_v = rand() % 75 + 75; // 75-150
 
-            this->id = c;
-            this->velocity = 1000000 / (random_v * VELOCITY_MULTIPLIER);
+            // this->id = c;
+            // this->velocity = 1000000 / (random_v * VELOCITY_MULTIPLIER);
+            car_finished_road = true;
+            break;
         }
-        cout << ". ";
+        // cout << ". ";
         return;
     }
 };
@@ -262,39 +266,17 @@ void print_board(char *matrix[BOARD_WIDTH])
             for (int j = 0; j < BOARD_WIDTH; j++)
             {
                 cout << matrix[i][j] << " ";
-                usleep(3);
+                usleep(1000000 / FPS);
             }
             cout << endl;
         }
     }
 }
 
-// void spawn_cars_road2(char *matrix[BOARD_WIDTH])
-// {
-//     int c = 65; //90
-//     int random_v;
-
-//     while (true)
-//     {
-//         c++;
-//         if (c > 90)
-//             c = 65;
-
-//         random_v = rand() % 150 + 50;
-//         // sleep(random_v/200);
-
-//         Car *temp_car = new Car(c, random_v, matrix);
-//         thread t_temp(&Car::drive2, temp_car);
-
-//         t_temp.join();
-//     }
-// }
-
 void print_board_curses(char *matrix[BOARD_WIDTH])
 {
     const int WIDTH_MULTIPLIER = 3;
 
-    initscr();
     // start_color();
     // init_pair(1, COLOR_GREEN, COLOR_BLACK); //backgroundprint_board_curses
     // init_pair(2, COLOR_RED, COLOR_BLACK); //roads
@@ -315,10 +297,10 @@ void print_board_curses(char *matrix[BOARD_WIDTH])
     }
 
     // attron(COLOR_PAIR(2));
-    //roads, cars
+    // roads, cars
     while (do_work)
     {
-        for (int i = LEFT_CORNER + 1; i <= RIGHT_CORNER; i++) //go right
+        for (int i = LEFT_CORNER + 1; i <= RIGHT_CORNER; i++) // go right
         {
             mvprintw(0, i * WIDTH_MULTIPLIER, "%c", matrix[0][i]); // =     move(0,0);     printw("%d", c);
             mvprintw(i, 0, "%c", matrix[i][0]);                    // =     move(0,0);     printw("%d", c);
@@ -344,34 +326,10 @@ void print_board_curses(char *matrix[BOARD_WIDTH])
             mvprintw(LEFT_CORNER, i * WIDTH_MULTIPLIER, "%c", matrix[LEFT_CORNER][i]);
         }
         refresh();
+        usleep(1000000 / FPS);
     }
     // attroff(COLOR_PAIR(1));
-    endwin();
-    return;
-}
 
-void ncurses_print()
-{
-    initscr();
-
-    while (do_work)
-    {
-
-        for (int k = 20; k < 120; k++)
-        {
-            if (k == 119)
-                k = 0;
-            for (int i = 0; i < 20; i++)
-            {
-                for (int j = 0; j < 20; j++)
-                {
-                    mvprintw(j, i, "%c", k); // =     move(0,0);     printw("%d", c);
-                }
-                refresh();
-            }
-        }
-    }
-    endwin();
     return;
 }
 
@@ -379,14 +337,33 @@ void wait_for_esc()
 {
     while (do_work)
     {
-        if (cin.get() == 27)
+        if (getch() == 27)
             do_work = false;
     }
     return;
 }
 
+void car2_manager(char *matrix[BOARD_WIDTH])
+{
+    while (do_work)
+    {
+        int rand_time = rand() % 25 + 1;
+        for(int i = 0; i < rand_time; i++){
+            if(!do_work) break;
+            sleep(1);
+            
+        }
+        if(!do_work) break;
+
+        Car *temp_car = new Car(rand() % 25 + 65, rand() % 80 + 50, matrix);
+        thread t_car(&Car::drive2, temp_car);
+        t_car.join();
+    }
+}
+
 int main()
 {
+    initscr();
     srand(time(0));
 
     char **matrix;
@@ -403,60 +380,39 @@ int main()
         }
     }
 
+    thread t_cars1[CARS_NR1];
+    thread t_cars2[CARS_NR2];
     thread t_printer(print_board_curses, matrix);
 
-    Car *c1 = new Car('1', 80, matrix);
-    thread t_c1(&Car::drive, c1);
+    for (int i = 0; i < CARS_NR1; i++)
+    {
+        Car *temp_car = new Car('0' + i, 80, matrix);
+        t_cars1[i] = thread(&Car::drive, temp_car);
+    }
 
-    Car *c2 = new Car('2', 80, matrix);
-    thread t_c2(&Car::drive, c2);
-
-    Car *c3 = new Car('3', 80, matrix);
-    thread t_c3(&Car::drive, c3);
-
-    Car *c4 = new Car('4', 80, matrix);
-    thread t_c4(&Car::drive, c4);
-
-    Car *c5 = new Car('5', 80, matrix);
-    thread t_c5(&Car::drive, c5);
-
-    Car *ca = new Car('A', 130, matrix);
-    thread t_ca(&Car::drive2, ca);
-
-    Car *cb = new Car('B', 120, matrix);
-    thread t_cb(&Car::drive2, cb);
-
-    Car *cc = new Car('C', 90, matrix);
-    thread t_cc(&Car::drive2, cc);
-
-    Car *cd = new Car('D', 80, matrix);
-    thread t_cd(&Car::drive2, cd);
-
-    Car *ce = new Car('E', 100, matrix);
-    thread t_ce(&Car::drive2, ce);
+    for (int i = 0; i < CARS_NR2; i++)
+    {
+        t_cars2[i] = thread(car2_manager, matrix);
+    }
 
     thread t_escape(wait_for_esc);
 
     t_escape.join();
     t_printer.join();
-    t_c1.join();
-    t_c2.join();
-    t_c3.join();
-    t_c4.join();
-    t_c5.join();
 
-    t_ca.join();
-    t_cb.join();
-    t_cc.join();
-    t_cd.join();
-    t_ce.join();
-
-    while (do_work)
+    for (int i = 0; i < CARS_NR1; i++)
     {
+        t_cars1[i].join();
+    }
+
+    for (int i = 0; i < CARS_NR2; i++)
+    {
+        t_cars2[i].join();
     }
 
     delete matrix;
     matrix = nullptr;
 
+    endwin();
     return 0;
 }
